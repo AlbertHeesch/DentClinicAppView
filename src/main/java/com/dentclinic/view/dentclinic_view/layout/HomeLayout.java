4f2880e1985f7089e271c981b24403515f41d090
aheesch <oddajgrzanki@gmail.com>
2023-05-26 15:29:28 +0200

    Tabs mechanism has been added

--- src/main/java/com/dentclinic/view/dentclinic_view/layout/HomeLayout.java
+++ src/main/java/com/dentclinic/view/dentclinic_view/layout/HomeLayout.java
@@ -4,11 +4,21 @@ import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
-import com.vaadin.flow.component.tabs.TabsVariant;
+import com.vaadin.flow.spring.annotation.UIScope;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
+@UIScope
 public class HomeLayout extends AppLayout {
+
+    private Tab bookTab = new Tab("Book");
+    private Tab updateTab = new Tab("Update");
+    private Tabs tabs = new Tabs(bookTab, updateTab);
 
     public HomeLayout() {
         H1 title = new H1("DentClinicApp");
@@ -16,21 +26,58 @@ public class HomeLayout extends AppLayout {
                 .set("font-size", "var(--lumo-font-size-l)")
                 .set("left", "var(--lumo-space-l)")
                 .set("margin", "0")
-                .set("position", "sticky");
+                .set("position", "sticky")
+                .set("color", "white");
 
-        Tab bookTab = new Tab("Book");
-        Tab updateTab = new Tab("Update");
+        bookTab.getStyle().set("color", "white");
+        updateTab.getStyle().set("color", "white");
 
-        Tabs tabs = new Tabs(bookTab, updateTab);
-        tabs.addThemeVariants(TabsVariant.LUMO_CENTERED);
+        HorizontalLayout itemsLayout = new HorizontalLayout(title, tabs);
+        itemsLayout.setWidth("80%");
+        itemsLayout.setAlignSelf(FlexComponent.Alignment.CENTER, title);
+        itemsLayout.setAlignSelf(FlexComponent.Alignment.CENTER, tabs);
+        itemsLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
 
-        HorizontalLayout layout = new HorizontalLayout(title, tabs);
-        layout.setWidth("100%");
+        HorizontalLayout topLayout1 = new HorizontalLayout(itemsLayout);
+        topLayout1.getStyle().set("background", "#282A35");
+        topLayout1.setWidth("100%");
+        topLayout1.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
 
-        layout.setAlignSelf(FlexComponent.Alignment.CENTER, title);
-        layout.setAlignSelf(FlexComponent.Alignment.CENTER, tabs);
-        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+        Select<String> citySelect = new Select<>("1", "2", "3");
+        citySelect.setPlaceholder("City");
+        Select<String> dateSelect = new Select<>("1", "2", "3");
+        dateSelect.setPlaceholder("Date");
+        Select<String> timeSelect = new Select<>("1", "2", "3");
+        timeSelect.setPlaceholder("Time");
+        Select<String> filterSelect = new Select<>("1", "2", "3");
+        filterSelect.setPlaceholder("Filter");
 
-        addToNavbar(layout);
+        HorizontalLayout selectItemsLayout = new HorizontalLayout(citySelect, dateSelect, timeSelect, filterSelect);
+        selectItemsLayout.setWidth("80%");
+
+        HorizontalLayout topLayout2 = new HorizontalLayout(selectItemsLayout);
+        topLayout2.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        topLayout2.setSizeFull();
+        topLayout2.getStyle()
+                .set("background", "#E7E9EB")
+                .set("padding", "2px 5px 2px 0px");
+
+        VerticalLayout verticalLayout = new VerticalLayout(topLayout1, topLayout2);
+        verticalLayout.setPadding(false);
+        verticalLayout.setSpacing(false);
+
+        addToNavbar(verticalLayout);
+    }
+
+    public Tab getBookTab() {
+        return bookTab;
+    }
+
+    public Tab getUpdateTab() {
+        return updateTab;
+    }
+
+    public Tabs getTabs() {
+        return tabs;
     }
 }
--- src/main/java/com/dentclinic/view/dentclinic_view/views/HomeView.java
+++ src/main/java/com/dentclinic/view/dentclinic_view/views/HomeView.java
@@ -1,36 +1,75 @@
 package com.dentclinic.view.dentclinic_view.views;
 
+import com.dentclinic.view.dentclinic_view.domain.Dentist;
 import com.dentclinic.view.dentclinic_view.layout.HomeLayout;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.virtuallist.VirtualList;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.spring.annotation.UIScope;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@UIScope
 @PageTitle("Home | DentClinicApp")
 @Route(value="home", layout = HomeLayout.class)
 @AnonymousAllowed
 public class HomeView extends VerticalLayout {
 
-    public HomeView()
+    private HomeLayout homeLayout;
+    private VirtualList<Dentist> virtualList = new VirtualList<>();
+    private VerticalLayout listLayout = new VerticalLayout(virtualList);
+    private Label specialistsLabel = new Label("Our specialists");
+    private VerticalLayout h2Layout = new VerticalLayout(specialistsLabel);
+
+    public HomeView( @Autowired HomeLayout homeLayout)
     {
+        this.homeLayout = homeLayout;
         addClassName("home-view");
-        setSizeFull();
+        setHeight("100%");
+        setWidth("100%");
         setAlignItems(Alignment.CENTER);
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        HorizontalLayout topLayout = new HorizontalLayout();
-        for(int i =0; i<50; i++) {
-            add(new HorizontalLayout((new Label("SAMPLEEEE\n"))));
+        getStyle().set("background-color", "blue");
+
+        specialistsLabel.getStyle().set("font-weight", "bold");
+
+        h2Layout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        h2Layout.setPadding(false);
+        h2Layout.getStyle().set("background-color", "orange");
+        h2Layout.setWidth("81%");
+        h2Layout.setHeight("4%");
+
+        List<Dentist> dentistList = new ArrayList<>();
+        for(int i=0; i<100; i++) {
+            dentistList.add(new Dentist(1L, "Name1", "Surname1", LocalDate.of(2000, 1, 1)));
         }
-        add(createTopLayout());
+        virtualList.setItems(dentistList);
+        listLayout.setWidth("81%");
+        listLayout.setHeight("100%");
+        listLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        listLayout.getStyle().set("background-color", "green");
+
+        setContent(homeLayout.getTabs().getSelectedTab());
+        homeLayout.getTabs().addSelectedChangeListener(event -> setContent(event.getSelectedTab()));
     }
 
-    private HorizontalLayout createTopLayout() {
-      HorizontalLayout topLayout = new HorizontalLayout();
+    private void setContent(Tab tab) {
+        removeAll();
 
-        topLayout.setHeight("40px");
-
-        return topLayout;
+        if(tab.equals(homeLayout.getBookTab())) {
+            add(h2Layout, listLayout);
+        } else {
+            //TODO update functionality
+        }
     }
 }
