6a6e49762c5fbddd774190deec56bc10b4e21720
aheesch <a.heesch@kcmrm.pl>
2023-06-26 14:28:43 +0200

    HomeView has been updated with min height and width

--- src/main/java/com/dentclinic/view/dentclinic_view/views/HomeView.java
+++ src/main/java/com/dentclinic/view/dentclinic_view/views/HomeView.java
@@ -2,6 +2,7 @@ package com.dentclinic.view.dentclinic_view.views;
 
 import com.dentclinic.view.dentclinic_view.layout.HomeLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -15,6 +16,9 @@ import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.util.*;
+
 @Component
 @UIScope
 @PageTitle("Home | DentClinicApp")
@@ -23,11 +27,12 @@ import org.springframework.stereotype.Component;
 public class HomeView extends VerticalLayout {
 
     private HomeLayout homeLayout;
-    private Label specialistsLabel = new Label("Our specialists");
-    private VerticalLayout h2Layout = new VerticalLayout(specialistsLabel);
+    private H2 specialistsLabel = new H2("Our specialists");
+    private HorizontalLayout h2Layout = new HorizontalLayout(specialistsLabel);
     private TextField surnameField = new TextField("Surname");
     private TextField peselField = new TextField("PESEL");
     private Button findButton = new Button("Find");
+    private List<String> list = List.of("Day1", "Day2", "Day3", "Day4", "Day5");
 
     public HomeView( @Autowired HomeLayout homeLayout)
     {
@@ -35,16 +40,14 @@ public class HomeView extends VerticalLayout {
         addClassName("home-view");
         setMinHeight("100%");
         setHeight("full");
-        setWidth("100%");
-        setAlignItems(Alignment.CENTER);
-        getStyle().set("background-color", "blue");
+        setWidth("80%");
+        getStyle().set("background-color", "white")
+                .set("padding", "0px 10% 5% 10%");
 
         specialistsLabel.getStyle().set("font-weight", "bold");
 
-        h2Layout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
         h2Layout.setPadding(false);
-        h2Layout.getStyle().set("background-color", "orange");
-        h2Layout.setWidth("81%");
+        h2Layout.setWidth("100%");
         h2Layout.setHeight("4%");
 
         setContent(homeLayout.getTabs().getSelectedTab());
@@ -57,18 +60,62 @@ public class HomeView extends VerticalLayout {
         if(tab.equals(homeLayout.getBookTab())) {
             add(h2Layout);
             for(int i=0; i<100; i++) {
-                Label sampleLabel = new Label("Sample Label" + i);
-                Button bookButton = new Button("Book");
-                HorizontalLayout horizontalLayout = new HorizontalLayout(sampleLabel, bookButton);
-                horizontalLayout.setWidth("81%");
-                horizontalLayout.setHeight("100%");
-//                ho.setAlignItems(FlexComponent.Alignment.CENTER);
+                Label sampleLabel = new Label("Name Surname" + i);
+                sampleLabel.getStyle().set("color", "white");
+
+                VerticalLayout dataLayout = new VerticalLayout(sampleLabel);
+                dataLayout.getStyle()
+                        .set("background-color", "#00c3a5")
+                        .set("border-radius", "25px");
+                dataLayout.setMinWidth("400px");
+
+                HorizontalLayout horizontalLayout = new HorizontalLayout(dataLayout);
+                horizontalLayout.setMinWidth("570px");
+                horizontalLayout.setMinHeight("280px");
                 horizontalLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
-                if(i%2 == 0){
-                    horizontalLayout.getStyle().set("background-color", "green");
-                } else {
-                    horizontalLayout.getStyle().set("background-color", "pink");
+                horizontalLayout.getStyle()
+                        .set("background", "#E7E9EB")
+                        .set("border-radius", "25px");
+
+                VerticalLayout buttonsGroupLayout = new VerticalLayout();
+                buttonsGroupLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+                buttonsGroupLayout.setWidth("60%");
+                buttonsGroupLayout.setHeight("100%");
+                buttonsGroupLayout.setSpacing(false);
+
+                HorizontalLayout buttonsLayout = new HorizontalLayout();
+                buttonsLayout.setWidth("100%");
+                buttonsLayout.setHeight("85%");
+                buttonsLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+                buttonsLayout.getStyle().set("padding", "5px 3px 5 px 3px");
+
+                Button leftButton = new Button("<");
+                leftButton.setMinHeight("220px");
+                leftButton.setWidth("5%");
+                buttonsLayout.add(leftButton);
+                leftButton.setEnabled(false);
+
+                for(int j=0; j<list.size(); j++) {
+                    VerticalLayout dateButtonsLayout = new VerticalLayout(new Label(LocalDate.now().plusDays(j).getDayOfMonth() + "\n" + String.valueOf(LocalDate.now().getMonth()).substring(0,3)));
+                    for(int x=0; x<4; x++) {
+                        Button bookButton = new Button("Book");
+                        dateButtonsLayout.add(bookButton);
+                    }
+                    dateButtonsLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+                    dateButtonsLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+                    dateButtonsLayout.setSpacing(false);
+                    dateButtonsLayout.setWidth("12%");
+                    dateButtonsLayout.setHeight("100%");
+                    buttonsLayout.add(dateButtonsLayout);
                 }
+                Button rightButton = new Button(">");
+                rightButton.setMinHeight("220px");
+                buttonsLayout.add(rightButton);
+                Button timeButton = new Button("More");
+                timeButton.setWidth("95%");
+                buttonsGroupLayout.add(buttonsLayout, timeButton);
+                horizontalLayout.add(buttonsGroupLayout);
+
                 add(horizontalLayout);
             }
         } else {
